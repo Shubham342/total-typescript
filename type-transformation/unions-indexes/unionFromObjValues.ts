be29253be00ed96f350f8ed92163752b1d192b81
SHUBHAM <shubhamsingla0809@gmail.com>
2024-02-08 19:54:52 +1100

    Some more union types for obj, array and discrimanated unions

---
+++ type-transformation/unions-indexes/unionFromObjValues.ts
@@ -0,0 +1,33 @@
+import { Equal, Expect } from "../../helpers/type-utils";
+
+export const programModeEnumMap = {
+  GROUP: "group",
+  ANNOUNCEMENT: "announcement",
+  ONE_ON_ONE: "1on1",
+  SELF_DIRECTED: "selfDirected",
+  PLANNED_ONE_ON_ONE: "planned1on1",
+  PLANNED_SELF_DIRECTED: "plannedSelfDirected",
+} as const;
+
+type ProgramModeEnumMap = typeof programModeEnumMap;
+
+export type IndividualProgram = Extract<
+  ProgramModeEnumMap[keyof ProgramModeEnumMap],
+  "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
+>;
+
+// Better way
+type IndividualProgram2 = ProgramModeEnumMap[
+  | "ONE_ON_ONE"
+  | "SELF_DIRECTED"
+  | "PLANNED_ONE_ON_ONE"
+  | "PLANNED_SELF_DIRECTED"];
+
+type tests = [
+  Expect<
+    Equal<
+      IndividualProgram,
+      "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
+    >
+  >
+];
