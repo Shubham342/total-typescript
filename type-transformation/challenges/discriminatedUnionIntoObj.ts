79f1843dbade0700b351ea218871647df1ae4112
SHUBHAM <shubhamsingla0809@gmail.com>
2024-02-20 21:22:34 +1100

    challenges related to type transformation

---
+++ type-transformation/challenges/discriminatedUnionIntoObj.ts
@@ -0,0 +1,34 @@
+import { Equal, Expect } from "../../helpers/type-utils";
+
+type Route =
+  | {
+      route: "/";
+      search: {
+        page: string;
+        perPage: string;
+      };
+    }
+  | { route: "/about" }
+  | { route: "/admin" }
+  | { route: "/admin/users" };
+
+type RoutesObject = {
+  [T in Route as T["route"]]: T extends { search: infer S } ? S : never;
+};
+
+type tests = [
+  Expect<
+    Equal<
+      RoutesObject,
+      {
+        "/": {
+          page: string;
+          perPage: string;
+        };
+        "/about": never;
+        "/admin": never;
+        "/admin/users": never;
+      }
+    >
+  >
+];
---
+++ type-transformation/challenges/transformPathIntoString.ts
@@ -0,0 +1,20 @@
+import { Equal, Expect } from "../../helpers/type-utils";
+import { Split } from "ts-toolbelt/out/String/Split";
+
+type UserPath = "/users/:id";
+
+type UserOrganisationPath = "/users/:id/organisations/:organisationId";
+
+type ExtractPathParams<T extends string> = {
+  [K in Split<T, "/">[number] as K extends `:${infer P}` ? P : never]: string;
+};
+
+type tests = [
+  Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
+  Expect<
+    Equal<
+      ExtractPathParams<UserOrganisationPath>,
+      { id: string; organisationId: string }
+    >
+  >
+];
